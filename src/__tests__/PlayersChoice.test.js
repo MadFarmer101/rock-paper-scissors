ffa9c6b51f04050bd6898544088a54d7c142709e
Janko <mailodjanka@gmail.com>
2020-03-03 12:01:37 +0100

    Adding component tests for a ComputersChoice component

--- src/__tests__/PlayersChoice.test.js
+++ src/__tests__/PlayersChoice.test.js
@@ -5,17 +5,17 @@ import PlayersChoice from "../PlayersChoice";
 describe("PlayersChoice component", () => {
   const wrapper = mount(<PlayersChoice />);
 
-  it("Player chooses rock", () => {
+  it("Player chooses rock item, an image of a rock is displayed", () => {
     wrapper.setProps({ item: 'rock' })
      expect(wrapper.find('.player-image').prop('src')).toEqual('rock.png');
   });
 
-  it("Player chooses paper", () => {
+  it("Player chooses paper item, an image of a paper is displayed", () => {
     wrapper.setProps({ item: 'paper' })
      expect(wrapper.find('.player-image').prop('src')).toEqual('paper.png');
   });
 
-  it("Player chooses scissors", () => {
+  it("Player chooses scissors item, an image of a scissors is displayed", () => {
     wrapper.setProps({ item: 'scissors' })
      expect(wrapper.find('.player-image').prop('src')).toEqual('scissors.png');
   });
