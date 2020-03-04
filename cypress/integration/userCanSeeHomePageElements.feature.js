097296510f117f62bdaf10c8e20f807d24e4afe1
Janko <mailodjanka@gmail.com>
2020-03-04 18:39:30 +0100

    Adding cypress tests to see if the match elements are not visible on the page and if they become visible after pressing a start game button

--- cypress/integration/userCanSeeHomePageElements.feature.js
+++ cypress/integration/userCanSeeHomePageElements.feature.js
@@ -16,18 +16,46 @@ describe('User can see all the elements on the Home page', () => {
         cy.get('#title').should('contain', 'Rock Paper Scissors')
     })
 
+    it('Does not display a rock button', () => {
+        cy.get('#rock').should('not.exist');
+    })
+
+    it('Does not display a paper button', () => {
+        cy.get('#paper').should('not.exist');
+    })
+
+    it('Does not display a scissors button', () => {
+        cy.get('#scissors').should('not.exist');
+    })
+})
+
+describe('User can see match elements after pressing start game button', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.get('#start').click()
+    })
+
     it('Displays a rock button', () => {
-        cy.get('#start').click()
         cy.get('#rock').should('exist');
     })
 
     it('Displays a paper button', () => {
-        cy.get('#start').click()
         cy.get('#paper').should('exist');
     })
 
     it('Displays a scissors button', () => {
-        cy.get('#start').click()
         cy.get('#scissors').should('exist');
+    })
+
+    it('Displays a title', () => {
+        cy.get('#title').should('contain', 'Rock Paper Scissors')
+    })
+
+    it('Displays the game rules', () => {
+        cy.get('#rules').should('contain', 'Paper beats Rock')
+    })
+
+    it('Does not display a scissors button', () => {
+        cy.get('#start').should('not.exist');
     })
 })
