dac0fabe3853dcd9e5581f51376ce1a5fb130262
Janko <mailodjanka@gmail.com>
2020-03-04 18:24:09 +0100

    Rewritting the UserCanSeeHomePageElements and PlayingGame tests so it fits the new design

--- cypress/integration/userCanSeeHomePageElements.feature.js
+++ cypress/integration/userCanSeeHomePageElements.feature.js
@@ -4,29 +4,30 @@ describe('User can see all the elements on the Home page', () => {
       cy.visit('/');
     })
 
+    it('Displays a start game button', () => {
+        cy.get('#start').should('exist');
+    })
+
+    it('Displays the game rules', () => {
+        cy.get('#rules').should('contain', 'Paper beats Rock')
+    })
+
     it('Displays a title', () => {
         cy.get('#title').should('contain', 'Rock Paper Scissors')
     })
 
     it('Displays a rock button', () => {
+        cy.get('#start').click()
         cy.get('#rock').should('exist');
     })
 
     it('Displays a paper button', () => {
+        cy.get('#start').click()
         cy.get('#paper').should('exist');
     })
 
     it('Displays a scissors button', () => {
+        cy.get('#start').click()
         cy.get('#scissors').should('exist');
     })
-  
-    it('Displays a start game button', () => {
-        cy.get('#game-button').should('exist');
-    })
-        
-    it('Displays the game rules', () => {
-        cy.get('#rules').should('contain', 'Paper beats Rock')
-    })
-
-
 })
